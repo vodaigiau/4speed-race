20abd89f3c1be1ce0374b39dee1a6c665f8ad116
vodaigiau <vorich1502@gmail.com>
2024-06-09 19:29:01 +0700

    setup code, config and change some scss files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms', 'next'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off'
+  },
 }
