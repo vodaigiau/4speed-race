f272cd98565bb37c5ef844d4edcd5316954b1446
vodaigiau <vorich1502@gmail.com>
2024-06-10 14:37:48 +0700

    build footer

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Footer, Media } from '../../../../payload/payload-types'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
+import { Button } from '../../Button'
+import { Gutter } from '../../Gutter'
+
+import classes from './index.module.scss'
+
+const FooterComponent = ({ footer }: { footer: Footer }) => {
+  const pathname = usePathname()
+  const navItems = footer?.navItems || []
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map(inclusions => (
+            <li key={inclusions.title}>
+              <Image
+                src={inclusions.icon}
+                alt={inclusions.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+              <h5 className={classes.title}>{inclusions.title}</h5>
+              <p>{inclusions.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="4speed race-black.svg" alt="logo" width={200} height={100} />
+            </Link>
+            <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinksItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterComponent
