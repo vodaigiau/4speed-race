4ee62d850a0252132999c1b64bbd432c4e8ed33f
vodaigiau <vorich1502@gmail.com>
2024-06-10 14:57:39 +0700

    Build login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import LoginForm from './LoginForm'
 
 import classes from './index.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Login() {
   await getMeUser({
@@ -15,11 +16,33 @@ export default async function Login() {
   })
 
   return (
-    <Gutter className={classes.login}>
-      <RenderParams className={classes.params} />
-      <h1>Log in</h1>
-      <LoginForm />
-    </Gutter>
+    <section className={classes.login}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/4speed race-white.svg"
+            alt="logo"
+            width={100}
+            height={10}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Welcome to our Auditorium</h3>
+            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30}/>
+          </div>
+
+          <p>Please Login Here</p>
+
+          <LoginForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
